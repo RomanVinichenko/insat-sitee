26334cf915a22dc5587946aa6fa011355e5a960a
RomanVinichenko <chicagezzz@gmail.com>
2022-07-05 14:27:25 +0300

    edit pictures +++

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,5 @@
 function pic() {
-  const bgm = ['https://github.com/RomanVinichenko/smidige/blob/master/app/images/top.jpg', 'https://github.com/RomanVinichenko/smidige/blob/master/app/images/top2.jpg'];
+  const bgm = ['https://raw.githubusercontent.com/RomanVinichenko/smidige/master/app/images/top.jpg', 'https://raw.githubusercontent.com/RomanVinichenko/smidige/master/app/images/top2.jpg'];
 
   $('.top').css({
     'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ')',
