0ec2dbb4ba10aa9cc04ff602f2d649131808739a
RomanVinichenko <chicagezzz@gmail.com>
2022-07-05 14:21:20 +0300

    edit pictures +

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,5 @@
 function pic() {
-  const bgm = ['app/images/top.jpg', 'app/images/top2.jpg'];
+  const bgm = ['blob/master/app/images/top.jpg', 'blob/master/app/images/top2.jpg'];
 
   $('.top').css({
     'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ')',
