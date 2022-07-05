2ba90a78a43f18819e6a112d88bf1deb33836187
RomanVinichenko <chicagezzz@gmail.com>
2022-07-05 14:05:39 +0300

    start project

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,8 @@
-$(function () {});
+function pic() {
+  const bgm = ['../images/top.jpg', '../images/top2.jpg'];
+
+  $('.top').css({
+    'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ')',
+  });
+}
+pic();
