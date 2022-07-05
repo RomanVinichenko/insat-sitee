e1d75dab34ca79dc936bb7b44c3bd63e541580b7
RomanVinichenko <chicagezzz@gmail.com>
2022-07-05 14:23:57 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function pic() {
-  const bgm = ['blob/master/app/images/top.jpg', 'blob/master/app/images/top2.jpg'];
+  const bgm = ['https://github.com/RomanVinichenko/smidige/blob/master/app/images/top.jpg', 'https://github.com/RomanVinichenko/smidige/blob/master/app/images/top2.jpg'];
 
   $('.top').css({
     'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ')',
