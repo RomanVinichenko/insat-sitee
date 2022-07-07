48f7f352e8e56b7a371e041e676ef46af36841e1
RomanVinichenko <chicagezzz@gmail.com>
2022-07-07 14:12:05 +0300

    added included

--- app/js/index.js
+++ app/js/index.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
     slidesToScroll: 1,
 });
 });
+
+let includes = document.getElementsByTagName('include');
+for(var i=0; i<includes.length; i++){
+  let include = includes[i];
+  load_file(includes[i].attributes.src.value, function(text){
+    include.insertAdjacentHTML('afterend', text);
+    include.remove();
+  });
+}
+function load_file(filename, callback) {
+  fetch(filename).then(response => response.text()).then(text => callback(text));
+}
