7b159ddf5ac5f753376c4c1ba4e56d17c0d6fa10
RomanVinichenko <chicagezzz@gmail.com>
2022-07-09 16:47:37 +0300

    engineering

--- app/js/index.js
+++ app/js/index.js
@@ -2,30 +2,41 @@ function pic() {
   const bgm = ['https://raw.githubusercontent.com/RomanVinichenko/smidige/master/app/images/top.jpg', 'https://raw.githubusercontent.com/RomanVinichenko/smidige/master/app/images/top2.jpg'];
 
   $('.top').css({
-    'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ')',
+    'background-image': 'url(' + bgm[Math.floor(Math.random() * bgm.length)] + ')',
   });
 }
+
 pic();
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.articles-slick').slick({
     arrows: false,
     slidesToShow: 3,
     infinite: true,
     slidesToScroll: 1,
+  });
 });
+
+$(document).ready(function () {
+  $('.scope__list').slick({
+    arrows: false,
+    slidesToShow: 2,
+    infinite: true,
+    slidesToScroll: 1,
+  });
 });
 
 // INCLUDES HTML FILES
 
 let includes = document.getElementsByTagName('include');
-for(var i=0; i<includes.length; i++){
+for (var i = 0; i < includes.length; i++) {
   let include = includes[i];
-  load_file(includes[i].attributes.src.value, function(text){
+  load_file(includes[i].attributes.src.value, function (text) {
     include.insertAdjacentHTML('afterend', text);
     include.remove();
   });
 }
+
 function load_file(filename, callback) {
   fetch(filename).then(response => response.text()).then(text => callback(text));
 }
