7926481bc4268b330537f0cd3da14eb11c98f138
RomanVinichenko <chicagezzz@gmail.com>
2022-07-14 11:42:14 +0300

    qa1

--- app/js/index.js
+++ app/js/index.js
@@ -8,13 +8,20 @@ function pic() {
 
 pic();
 
-$(document).ready(function () {
-  $('.articles-slick').slick({
-    arrows: false,
-    slidesToShow: 3,
-    infinite: true,
-    slidesToScroll: 1,
-  });
+// $(document).ready(function () {
+//   $('.articles-slick').slick({
+//     arrows: false,
+//     slidesToShow: 2,
+//     infinite: true,
+//     slidesToScroll: 1,
+//     speed: 300,
+//   });
+// });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 24,
 });
 
 $(document).ready(function () {
@@ -40,3 +47,49 @@ for (var i = 0; i < includes.length; i++) {
 function load_file(filename, callback) {
   fetch(filename).then(response => response.text()).then(text => callback(text));
 }
+
+// HEADER ACTIVE
+
+let header = document.querySelector(".header");
+
+window.addEventListener('scroll', function () {
+  let scrollPos = window.scrollY;
+
+  if (scrollPos > 10) {
+    header.classList.add("header--active");
+  } else {
+    header.classList.remove("header--active");
+  }
+  console.log(scrollPos)
+});
+
+$(function () {
+  $('.link__service, .section__link, .article__element, .expert__button')
+    .on('mouseenter', function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find('span').css({top: relY, left: relX})
+    })
+    .on('mouseout', function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find('span').css({top: relY, left: relX})
+    });
+  $('[href=#]').click(function () {
+    return false
+  });
+});
+
+$(document).ready(function () {
+
+  let scrollPos = window.scrollY;
+
+  if (scrollPos > 10) {
+    header.classList.add("header--active");
+
+  } else {
+    header.classList.remove("header--active");
+  }
+});
